7b0561ed89945e4b97a050b355c93ec2a9744cd3
Sven Keimpema <keimpje1234@gmail.com>
2026-02-03 22:31:51 +0100

    feat: initial commit

---
+++ backend/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.requestflow"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
